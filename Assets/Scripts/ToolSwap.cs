302751b3b828c5244a031a414824915705ea301c
iArctix <connortodd.ct@gmail.com>
2025-02-23 22:48:54 +0000

    Simple tool swapping

---
+++ Assets/Scripts/ToolSwap.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ToolSwap : MonoBehaviour
+{
+    public GameObject metalDetector; // Reference to the metal detector
+    public GameObject shovel;        // Reference to the shovel
+
+    void Update()
+    {
+        // Equip Metal Detector
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // Press 1 to equip metal detector
+        {
+            EquipTool(metalDetector, shovel);
+        }
+
+        // Equip Shovel
+        if (Input.GetKeyDown(KeyCode.Alpha2)) // Press 2 to equip shovel
+        {
+            EquipTool(shovel, metalDetector);
+        }
+    }
+
+    // Function to enable the selected tool and disable the other
+    void EquipTool(GameObject toolToEquip, GameObject toolToDisable)
+    {
+        toolToEquip.SetActive(true);   // Enable the tool to equip
+        toolToDisable.SetActive(false); // Disable the other tool
+    }
+}
